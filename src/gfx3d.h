163156b7de4a47686619cf23ec3e1663dc9e88a4
Comfrey29 <tiopepa37@gmail.com>
2025-10-15 21:01:04 +0200

    Update gfx3d.h

--- src/gfx3d.h
+++ src/gfx3d.h
@@ -1,15 +1,13 @@
 #ifndef GFX3D_H
 #define GFX3D_H
 
-#include <SDL2/SDL.h>
 #include <GL/gl.h>
-#include <GL/glu.h>
-#include <stdbool.h>
 
-bool GFX3D_Init(int width, int height);
-void GFX3D_Shutdown(void);
-void GFX3D_Clear(void);
-void GFX3D_Present(void);
-void GFX3D_SetCamera(float x, float y, float z, float pitch, float yaw);
+// Inicialitza OpenGL
+void Gfx_Init(int width, int height);
+
+// Dibuixa un cub amb textura (atlas)
+void Gfx_DrawCube(float x, float y, float z, GLuint texture,
+                  float u1, float v1, float u2, float v2);
 
 #endif
