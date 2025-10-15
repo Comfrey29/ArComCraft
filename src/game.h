412a7e7d11a6c4dac435837291f85bfbde6e704c
Comfrey29 <tiopepa37@gmail.com>
2025-10-15 15:13:28 +0200

    Update game.h

--- src/game.h
+++ src/game.h
@@ -1,1 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <stdbool.h>
+
+bool Game_Init(void);
+void Game_Shutdown(void);
+void Game_Update(void);
+void Game_Render(void);
+bool Game_Running(void);
+void Game_SetMode(int mode);
+
+#define GAME_MODE_CREATIVE 1
+
+#endif
 
