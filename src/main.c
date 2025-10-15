14dbdd1c0654bc2c7773b5d76dfaebb1df012ce3
Comfrey29 <tiopepa37@gmail.com>
2025-10-15 15:13:00 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "menu.h"
+#include "game.h"
+
+int main(void) {
+    printf("ArCom Creative+ Edition\n");
+
+    // Inicialitzar motor gràfic i joc
+    if (!Game_Init()) {
+        printf("Error inicialitzant el joc.\n");
+        return 1;
+    }
+
+    // Mostrar menú principal
+    Menu_Show();
+
+    // Bucle principal
+    while (Game_Running()) {
+        Game_Update();
+        Game_Render();
+    }
+
+    Game_Shutdown();
+    return 0;
+}
 
