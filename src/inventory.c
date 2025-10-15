d529ca0530ef694b0889f630215c26cdad544685
Comfrey29 <tiopepa37@gmail.com>
2025-10-15 15:15:23 +0200

    Update inventory.c

--- src/inventory.c
+++ src/inventory.c
@@ -1,1 +1,25 @@
+#include "inventory.h"
+#include "blocks.h"
+#include <stdio.h>
+
+static int inventory[MAX_BLOCKS] = {0};
+
+void Inventory_Init(void) {
+    for (int i = 0; i < MAX_BLOCKS; i++) inventory[i] = 0;
+}
+
+void Inventory_AddBlock(int blockId, int amount) {
+    if (blockId >= 0 && blockId < MAX_BLOCKS)
+        inventory[blockId] += amount;
+}
+
+void Inventory_Show(void) {
+    printf("Inventari:\n");
+    for (int i = 0; i < MAX_BLOCKS; i++) {
+        if (inventory[i] > 0) {
+            Block* b = Blocks_Get(i);
+            if (b) printf("%s x%d\n", b->name, inventory[i]);
+        }
+    }
+}
 
