53fd504d3d44b5c2b8c34d9428e6396020b85141
Comfrey29 <tiopepa37@gmail.com>
2025-10-15 15:19:12 +0200

    Update gfx.h

--- src/gfx.h
+++ src/gfx.h
@@ -1,1 +1,23 @@
+#ifndef GFX_H
+#define GFX_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdbool.h>
+
+// Inicialització i tancament
+bool GFX_Init(int width, int height);
+void GFX_Shutdown(void);
+
+// Dibuixar
+void GFX_Clear(void);
+void GFX_Present(void);
+
+// Carregar textures
+SDL_Texture* GFX_LoadTexture(const char* path);
+
+// Renderer accessible des d'altres fitxers
+extern SDL_Renderer* renderer;
+
+#endif
 
