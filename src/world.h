a9a8d76f21375404d137fd17c8982c1c35fe9be4
Comfrey29 <tiopepa37@gmail.com>
2025-10-15 21:16:09 +0200

    Update world.h

--- src/world.h
+++ src/world.h
@@ -1,6 +1,10 @@
-// src/world.h
 #ifndef WORLD_H
 #define WORLD_H
+
+// Inicialitza el món (de moment buit)
 void World_Init(void);
+
+// Actualitza el món (de moment buit)
 void World_Update(void);
+
 #endif
