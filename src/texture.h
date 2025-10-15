373893f128fe23bb1382071c5417df32e77faaad
Comfrey29 <tiopepa37@gmail.com>
2025-10-15 20:59:07 +0200

    Update texture.h

--- src/texture.h
+++ src/texture.h
@@ -5,6 +5,13 @@
 #include <SDL2/SDL_image.h>
 #include <GL/gl.h>
 
-GLuint Texture_Load(const char* filename);
+// ID de la textura única
+extern GLuint textureAtlas;
+
+// Funcions
+void Texture_LoadAtlas(const char* path);
+
+// Obtenir coordenades UV d’un bloc dins l’atlas
+void Texture_GetCoords(int tileX, int tileY, float* u1, float* v1, float* u2, float* v2);
 
 #endif
